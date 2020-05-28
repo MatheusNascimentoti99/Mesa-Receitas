496aba208b066e8932a554cae07fcb693ab0b450
matheusnascimentoti99 <matheusnascimentoti99@gmail.com>
2020-05-28 11:46:07 -0300

    CRUD de autor completo

--- mreceitas/model/Autor.js
+++ mreceitas/model/Autor.js
@@ -41,4 +41,45 @@ module.exports = class Autor {
             console.log('executou!');
         });
     }
+
+    delete(param, res) {
+        let sqlQry = `DELETE FROM autor where nickname = '${param.nickname}'`;
+        console.log(sqlQry);
+        var connection = dataBase.conectBD();
+        connection.query(sqlQry, function (error, results, fields) {
+            if (error) {
+                res.json(error);
+            }
+            else {
+                res. json({
+                    code: 'ok',
+                    results
+                });
+            }
+            connection.end();
+            console.log('executou!');
+        });
+    }
+
+    update(object, res){
+        var {nome, nickname, nascimento} = object
+        let sqlQry = `UPDATE autor set nome = ?, nickname = ?, nascimento = ? where nickname = '${object.oldNickname}'`;
+        console.log(sqlQry);
+        let values = [];
+        values.push(nome, nickname, nascimento);
+        var connection = dataBase.conectBD();
+        connection.query(sqlQry,values, function (error, results, fields) {
+            if (error) {
+                res.json(error);
+            }
+            else {
+                res. json({
+                    code: 'ok',
+                    results
+                });
+            }
+            connection.end();
+            console.log('executou!');
+        });
+    }
 }
